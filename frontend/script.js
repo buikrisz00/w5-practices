b1571226ea8424d9d392be683c3fff216224bd98
Krisztian Bui <buikrisz00@gmail.com>
2022-02-16 18:24:04 +0100

    Js fun time

--- frontend/script.js
+++ frontend/script.js
@@ -111,7 +111,7 @@ g.myMethod(); */
         }
     }
 } */
-
+/* 
 
 let h = {
     "anotherMethod": function (text) {
@@ -121,6 +121,91 @@ let h = {
 
 let i = h.anotherMethod("Yolo");
 console.log(i);
-console.log(h.anotherMethod("Yooolooo"));
+console.log(h.anotherMethod("Yooolooo")); */
+
+/* let myString = "mikkamakka";
+let mySecondString = myString;
+let myThirdString = "mikkamakka";
+
+console.log(myString);
+console.log(mySecondString);
+console.log(mySecondString === myString);
+console.log(myThirdString === myString); */
+
+/* let myObject = {
+    "myString": "mikkamakka"
+}
+
+let mySecondObject = myObject;
+
+mySecondObject.myString = "sdadsad";
+
+let myThirdObject = {
+    "myString": "mikkamakka"
+}
+
+let myFourthObject = {...myObject};
+myFourthObject.myString = "Vacskamati";
+
+
+console.log(mySecondObject === myObject);
+console.log(myFourthObject === myObject); */
+
+/* console.log("" === "");
+console.log({} === {}); */
+
+
+/* window.addEventListener("load", function () {
+
+    console.log("ez betöltődött");
+}) */
+
+function loadEvent() {
+    /* let rootElement = document.getElementById("root");
+    
+    let cardElement = function (movieReceive) {
+        return `
+        <div class="card">
+            <h2>${movieReceive.title}</h2>
+            <time>${movieReceive.year}</time>
+            <span>${movieReceive.rate}</span>
+        </div>
+        `;
+    }
+
+    for (const movieSend of movies) {
+        rootElement.insertAdjacentHTML("beforeend", cardElement(movieSend));
+    }
+    console.log(movies); */
+
+    let rootElement = document.getElementById("root");
+    
+    let cardElement2 = function (title, year, rate) {
+        return `
+        <div class="card">
+            <h2>${title}</h2>
+            <time>${year}</time>
+            <span>${rate}</span>
+        </div>
+        `;
+    }
+
+    let anotherFavoriteMovie = {
+        "title": "TlSb",
+        "year": 1991,
+        "rate": 7.0
+    }
+    rootElement.insertAdjacentHTML("beforeend", cardElement2(anotherFavoriteMovie.title, anotherFavoriteMovie.year, anotherFavoriteMovie.rate));
+
+    for (const movieSend of movies) {
+        rootElement.insertAdjacentHTML("beforeend", cardElement2(movieSend.title, movieSend.year, movieSend.rate));
+    }
+
+}
+
+window.addEventListener("load", loadEvent);
+
+
+
 
 
